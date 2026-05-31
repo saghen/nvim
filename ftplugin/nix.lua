251b5fc252ed501cfb3de7b2998e0adf713d56d8
Liam Dyer <liamcdyer@gmail.com>
2026-05-31 17:13:06 -0400

    fix(nix): disable auto-eval inputs

--- ftplugin/nix.lua
+++ ftplugin/nix.lua
@@ -1,13 +1,3 @@
 vim.treesitter.start()
-vim.lsp.config('nil_ls', {
-  settings = {
-    ['nil'] = {
-      formatting = { command = { 'nixfmt' } },
-      nix = {
-        maxMemoryMB = 8192, -- memory limit for evaluating inputs
-        flake = { autoArchive = true, autoEvalInputs = true },
-      },
-    },
-  },
-})
+vim.lsp.config('nil_ls', { settings = { ['nil'] = { formatting = { command = { 'nixfmt' } } } } })
 vim.lsp.enable('nil_ls')
